44bd921c76b72fdefff27a0d82dc022223394aa1
boyunJang <gobook1234@naver.com>
2022-11-20 23:48:42 +0900

    20221120

---
+++ 06.binary_search/binary_search_I/367.valid_perfect_score.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        low = 1
+        high = num
+        while low <= high:
+            mid = (low + high) // 2
+            if mid * mid == num: return True
+            if mid * mid < num:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return False
